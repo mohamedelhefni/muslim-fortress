41c6945d117d8430c9a2829651dd344d202ba4c8
Mohamed Elhefni <elhefnymohamed446@gmail.com>
2024-01-25 23:01:02 +0200

    feat: add support for PWA by using vite-pwa plugin

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
+import { VitePWA } from 'vite-plugin-pwa'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -7,6 +8,24 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [
     vue(),
+
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['beads.ico', 'logo.png', 'beads.svg'],
+      manifest: {
+        name: 'حصن المسلم',
+        short_name: 'حصن المسلم',
+        description: 'حصن المسلم',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/logo.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
